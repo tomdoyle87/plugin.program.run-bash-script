86bcbd68fbdcb98ece395601c8288fcde922b417
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2021-08-15 10:12:20 +0100

    fixed typos

--- addon.py
+++ addon.py
@@ -12,14 +12,13 @@ import subprocess
 import time
 
 # plugin constants
-__plugin__ = "Run Bash script from a program plugin "
+__plugin__ = "Run Bash script from a program plugin"
 __author__ = "tomdoyle87"
 __url__ = "https://github.com/tomdoyle87"
 __git_url__ = "https://github.com/tomdoyle87/plugin.program.run-bash-script"
 __credits__ = "tomdoyle87"
 __version__ = "0.0.1"
 
-os.system("/bin/echo -e '{0}\n{0}' | /usr/bin/sudo /usr/bin/passwd osmc" .format(Osmc))
-time.sleep(5)
 subprocess.Popen(["/home/osmc/bin/getstreamlist", "R"])
+time.sleep(5)
 dialog.ok("Streams updated")
